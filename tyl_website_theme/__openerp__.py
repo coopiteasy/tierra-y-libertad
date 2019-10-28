7a06613fcf5bf166add2a03fc44f5df8209e4e7f
Rémy Taymans <remytaymans@gmail.com>
2019-10-28 13:45:59 +0100

    [ADD] tyl_website_theme

---
+++ tyl_website_theme/__openerp__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Tierra y Libertad Website Theme",
+    "summary": """
+        Apply Tierra y Libretad design rules on website
+    """,
+    "description": """
+    """,
+    "author": "Tierra y Libertad, Coop It Easy SCRLfs",
+    "website": "https://github.com/coopiteasy/tierra-y-libertad",
+    "license": "AGPL-3",
+    "category": "Themes",
+    "version": "9.0.1.0.0",
+    "depends": ["website"],
+    "data": ["views/assets.xml"],
+}
